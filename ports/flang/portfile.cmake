a64139cb11aa87e1f13cf4eac7d95a0545228639
AlvinZhangH <260137639@qq.com>
2019-09-05 11:11:26 +0800

    fix2235issue-add_port_flang

---
+++ ports/flang/portfile.cmake
@@ -0,0 +1,26 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO flang-compiler/flang
+    REF flang_20190329
+    SHA512 860cdd74fd0714d80bbe5d70e041456198aee8f716b97191717b7b6533162f2ea4b88a4e61a5852bee38a8dfadcd62a3244ffd1bed684ce14183246bd13ae56a
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/absl TARGET_PATH share/absl)
+
+vcpkg_copy_pdbs()
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/abseil RENAME copyright)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share
+                    ${CURRENT_PACKAGES_DIR}/debug/include
+                    ${CURRENT_PACKAGES_DIR}/include/absl/copts
+                    ${CURRENT_PACKAGES_DIR}/include/absl/strings/testdata
+                    ${CURRENT_PACKAGES_DIR}/include/absl/time/internal/cctz/testdata)
